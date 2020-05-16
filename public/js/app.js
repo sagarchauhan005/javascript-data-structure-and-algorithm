5911f9bc6fe7f0fe58a3529bb2ec646109e85713
coolsagar005 <coolsagar005@gmail.com>
2020-05-16 18:23:22 +0530

    hash tables done

--- public/js/app.js
+++ public/js/app.js
@@ -483,7 +483,89 @@ console.log("isBalanced : "+myTree.isBalanced());
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+function hash(string, max) {
+  if (max > 0) {
+    var hash = 0;
 
+    for (var i = 0; i < string.length; i++) {
+      //console.log(string.charCodeAt(i));
+      hash += string.charCodeAt(i); //console.log(hash);
+    }
+
+    return hash % max;
+  }
+
+  return null;
+}
+
+var HashTable = function HashTable() {
+  var storage = [];
+  var limit = 14;
+
+  this.print = function () {
+    console.log(storage);
+  };
+
+  this.add = function (key, value) {
+    var index = hash(key, limit);
+
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      //console.log(storage);
+      var inserted = false;
+
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+
+      if (!inserted) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+
+  this.remove = function (key, value) {
+    var index = hash(key, limit);
+
+    if (storage[index].length === 1 && storage[0][0] === key) {
+      delete storage[index];
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          delete storage[index][i];
+        }
+      }
+    }
+  };
+
+  this.lookup = function (key) {
+    var index = hash(key, limit);
+
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i];
+        }
+      }
+    }
+  };
+};
+/*
+let ht = new HashTable();
+ht.add('beau', 'person');
+ht.add('fido', 'dog');
+ht.add('rex', 'dinosour');
+ht.add('tux', 'penguin');
+console.log("Print");
+console.log(ht.print());
+console.log(ht.lookup('rex'));
+*/
 
 /***/ }),
 
--- resources/views/pages/hash-tables.blade.php
+++ resources/views/pages/hash-tables.blade.php
@@ -2,23 +2,102 @@
 @section('content')
     <div class="row">
         <div id="about-code">
-            <h4><b>Sets</b></h4>
-            <p>A queue is another linear data structure which follows the FIFO principle. A queue allows two basic
-                operations on the collection. The first one is enqueue which allows us to add an item to the back of the
-                queue. The second one is dequeue which allows us to remove an item from the front of the queue</p>
-            <p>Below mentioned is the list of methods written.</p>
+            <h4><b>Hash Tables</b></h4>
+            <p>In programming language, a hash table is a data structure which is used to make an array associative. It
+                means we can use keys to map values instead of using an index. A hash table must use a hash function to
+                compute an index into an array of buckets or slots, from which the desired value can be found</p>
             <ul>
-                <li>Enqueue: add a queue</li>
-                <li>Dequeue: remove a queue</li>
-                <li>Latest: returns latest active queue</li>
-                <li>Size: returns size of the queue</li>
-                <li>isEmpty: checks if the queue is empty or not</li>
+                <li>Add: add a value</li>
+                <li>Remove: remove a value</li>
+                <li>Lookup: finds a value</li>
             </ul>
         </div>
 
         <div id="about-project">
             <pre>
 <code>
+
+function hash(string, max){
+    if(max>0){
+        var hash = 0;
+        for (let i = 0; i <\string.length ; i++) {
+            //console.log(string.charCodeAt(i));
+            hash += string.charCodeAt(i);
+            //console.log(hash);
+        }
+        return hash % max;
+    }
+    return null;
+}
+
+var HashTable = function(){
+
+    let storage = [];
+    let limit = 14;
+
+    this.print = function() {
+        console.log(storage);
+    };
+
+    this.add = function (key, value) {
+        var index = hash(key, limit);
+        if(storage[index]===undefined){
+            storage[index] = [
+                [key, value],
+            ];
+        }else{
+            //console.log(storage);
+            var inserted = false;
+            for (let i = 0; i <\storage[index].length ; i++) {
+                if(storage[index][i][0]===key){
+                    storage[index][i][1] = value;
+                    inserted = true;
+                }
+            }
+
+            if(!inserted){
+                storage[index].push([key, value]);
+            }
+        }
+    };
+
+    this.remove = function (key, value) {
+        var index = hash(key, limit);
+        if(storage[index].length===1 && storage[0][0]===key){
+            delete storage[index];
+        }else{
+            for (let i = 0; i <\storage[index].length ; i++) {
+                if(storage[index][i][0]===key){
+                    delete storage[index][i];
+                }
+            }
+        }
+    };
+
+    this.lookup = function (key) {
+        var index = hash(key, limit);
+        if(storage[index]===undefined){
+            return undefined;
+        }else{
+            for (let i = 0; i <\storage[index].length ; i++) {
+                if(storage[index][i][0]===key){
+                    return storage[index][i];
+                }
+            }
+        }
+    }
+
+};
+
+let ht = new HashTable();
+ht.add('beau', 'person');
+ht.add('fido', 'dog');
+ht.add('rex', 'dinosour');
+ht.add('tux', 'penguin');
+console.log("Print");
+console.log(ht.print());
+console.log(ht.lookup('rex'));
+
 </code>
             </pre>
         </div>
